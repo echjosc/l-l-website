d6ff2367a7b3d29512d23292124037043d4e1d05
Joseph Echeverria <josephecheverria@Josephs-MacBook-Pro.local>
2020-06-23 13:43:54 -0400

    Final Changes

---
+++ src/components/Announcement.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import "./sass/Announcement.scss"
+
+export default class Announcement extends Component {
+    render() {
+        return (
+            <div id="announcement">
+                <div class="attn">
+                    SERVING WESTCHESTER AND FAIRFIELD COUNTIES
+                </div>
+            </div>
+        )
+    }
+}
